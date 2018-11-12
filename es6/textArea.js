9b92c535fbd300181a5831260466f1f39a92447f
DESKTOP-S3DQCA1\Administrator <ridu196412@gmail.com>
2018-11-12 13:26:27 +0200

    add constructor

--- es6/textArea.js
+++ es6/textArea.js
@@ -1,14 +1,12 @@
-const TextBox = document.getElementById('textarea');
+TextBox = document.getElementById('textarea');
 class TextArea {
-
-    // constructor(maxrows, minrows) {
-    //     this.maxrows = maxrows;
-    //     this.minrows = minrows;
-    // }
-    Resize() {
-        textarea.addEventListener("input", function () {
-            let Maxrows = 5;
-            let Minrows = 2;
+    constructor(maxrows, minrows) {
+        this.Maxrows = maxrows
+        this.Minrows = minrows
+        this.resizeTextarea();
+    }
+    resizeTextarea() {
+        textarea.addEventListener("input", () => {
             let OverflowOn = 'auto'
             let OverflowOff = 'hidden'
             let TextBox = document.getElementById('textarea')
@@ -16,18 +14,19 @@ class TextArea {
             let Cols = TextBox.cols;
             let Arraytxt = EnteredText.split('\n');
             let Rows = Arraytxt.length;
+
             TextBox.style.overflow = OverflowOff
             for (let i = 0; i < Arraytxt.length; i++)
                 Rows += parseInt(Arraytxt[i].length / Cols);
-            if (Rows < Minrows) {
-                Rows = Minrows
+            if (Rows < this.Minrows) {
+                Rows = this.Minrows
             }
-            if (Rows > Maxrows) TextBox.rows = Maxrows, TextBox.style.overflow = OverflowOn;
+            if (Rows > this.Maxrows) TextBox.rows = this.Maxrows, TextBox.style.overflow = OverflowOn;
             else TextBox.rows = Rows;
         });
     }
 }
 
 let ExpandTextarea = new TextArea(5, 2);
-TextBox.addEventListener('input', ExpandTextarea.Resize);
+TextBox.addEventListener('input', ExpandTextarea.resizeTextarea());
 
