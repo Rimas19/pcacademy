4a97d9defdfc91cef5d1cb238d778f1a0ae7e3ea
DESKTOP-S3DQCA1\Administrator <ridu196412@gmail.com>
2018-11-12 16:28:02 +0200

    second bug fixed

--- es6/textArea.js
+++ es6/textArea.js
@@ -1,27 +1,29 @@
 const textBox = document.getElementById('textarea');
 class TextArea {
     constructor(maxrows, minrows) {
-        this.maxRows = maxrows
-        this.minRows = minrows
+        this.maxRows = maxrows;
+        this.minRows = minrows;
         this.resizeTextarea();
     }
     resizeTextarea() {
         textarea.addEventListener("input", () => {
-            let overflowOn = 'auto'
-            let overflowOff = 'hidden'
-            let textBox = document.getElementById('textarea')
+            const overflowOn = 'auto';
+            const overflowOff = 'hidden';
+            const textBox = document.getElementById('textarea')
             let enteredText = textBox.value;
             let cols = textBox.cols;
-            let arrayText = enteredText.split('\n');
+            const arrayText = enteredText.split('\n');
             let rows = arrayText.length;
-            textBox.style.overflow = overflowOff
+            textBox.style.overflow = overflowOff;
             for (let i = 0; i < arrayText.length; i++) {
                 rows += parseInt(arrayText[i].length / cols);
             }
-            if (rows > this.maxRows) {
-                textBox.rows = this.maxRows, textBox.style.overflow = overflowOn;
-            } else if (rows < this.minRows) {
+            if (rows < this.minRows || enteredText === '') {
                 rows = this.minRows
+            }
+            if (rows > this.maxRows) {
+                textBox.rows = this.maxRows;
+                textBox.style.overflow = overflowOn;
             }
             else {
                 textBox.rows = rows;
