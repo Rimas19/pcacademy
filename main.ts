15b5452637717fd755d063f10595864fbc1df1ca
DESKTOP-S3DQCA1\Administrator <ridu196412@gmail.com>
2018-11-19 09:25:02 +0200

    fixed

--- main.ts
+++ main.ts
@@ -5,8 +5,8 @@ import { ConsumCredit } from "./source/consumCredit/consumCredit";
 let fastResult = new FastCredit();
 document.getElementById("fast_btn").addEventListener('click', fastResult.calculateCredit);
 
-// let longResult = new LongCredit();
-// document.getElementById("btn").addEventListener('click', longResult.calculateCredit);
+let longResult = new LongCredit();
+document.getElementById("btn").addEventListener('click', longResult.calculateCredit);
 
-// let consumResult = new ConsumCredit();
-// document.getElementById("con_btn").addEventListener('click', consumResult.calculateCredit);
+let consumResult = new ConsumCredit();
+document.getElementById("con_btn").addEventListener('click', consumResult.calculateCredit);
