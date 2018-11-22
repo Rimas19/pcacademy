35293c2871d3ea2128749c01b03abec3a8df642a
DESKTOP-S3DQCA1\Administrator <ridu196412@gmail.com>
2018-11-22 12:00:26 +0200

    set global variable

--- monitor.js
+++ monitor.js
@@ -1,18 +1,21 @@
 let os = require('os');
 os.totalmem();
 os.freemem();
-let ramPercent = (100 -(os.freemem/os.totalmem)*100);
+let USAGE_PERCENT = 100;
+let TOTAL_PERCENT = 100;
+let LIMIT_USAGE_PERCENT = 15;
+let ramPercent = (TOTAL_PERCENT - (os.freemem / os.totalmem) * USAGE_PERCENT);
 let osu = require('os-utils');
 function intervalFunc() {
-    osu.cpuUsage(function(v){
-        if((v*100)<15){
-            console.log("\x1b[32m", 'CPU Usage (%): ' + (v*100) );
-        }else{
-            console.log("\x1b[31m", 'CPU Usage (%): ' + (v*100) );
+    osu.cpuUsage(function (v) {
+        if ((v * USAGE_PERCENT) < LIMIT_USAGE_PERCENT) {
+            console.log("\x1b[32m", 'CPU Usage (%): ' + (v * USAGE_PERCENT));
+        } else {
+            console.log("\x1b[31m", 'CPU Usage (%): ' + (v * USAGE_PERCENT));
         }
     });
-        console.log("\x1b[32m", 'RAM Usage (%): ' + (100 - (osu.freememPercentage()*100)) );
-        console.log('---------------------------------- ');
+    console.log("\x1b[32m", 'RAM Usage (%): ' + (TOTAL_PERCENT - (osu.freememPercentage() * USAGE_PERCENT)));
+    console.log('---------------------------------- ');
 }
-              
+
 setInterval(intervalFunc, 500);
