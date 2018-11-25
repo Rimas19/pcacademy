8577ce0768f19ce2bc04ebb6e4aaf5d51fbcc902
Rimas <ridu196412@gmail.com>
2018-11-25 18:28:27 +0200

    fixed

--- monitor.js
+++ monitor.js
@@ -6,13 +6,13 @@ let osu = require('os-utils');
 function intervalFunc() {
     osu.cpuUsage(function(v){
         if((v*100)<15){
-            console.log("\x1b[32m", 'CPU Usage (%): ' + (v*100) );
+            console.log("\x1b[32m", 'CPU Usage (%): ' + Math.round((v*100)) );
         }else{
-            console.log("\x1b[31m", 'CPU Usage (%): ' + (v*100) );
+            console.log("\x1b[31m", 'CPU Usage (%): ' + Math.round((v*100)) );
         }
     });
-        console.log("\x1b[32m", 'RAM Usage (%): ' + (100 - (osu.freememPercentage()*100)) );
-        console.log('---------------------------------- ');
+        console.log("\x1b[32m", 'RAM Usage (%): ' +Math.round((100 - (osu.freememPercentage()*100))) );
+        console.log("\x1b[37m",'---------------------------------- ');
 }
               
 setInterval(intervalFunc, 500);
