43eed4f781684cfe2f6c91604ef8bba4d9f2230b
DESKTOP-S3DQCA1\Administrator <ridu196412@gmail.com>
2018-12-03 12:58:42 +0200

    created  CRUD

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -2,36 +2,33 @@ const Invoice = require('../models/invoiceModel');
 const Supplier = require('../models/supplierModel');
 const Customer = require('../models/customerModel');
 const Project = require('../models/projectModel');
-const dateFormat = require('dateformat');
 const INVOICE_VAT = 21;
-const DATE =dateFormat(new Date(), "yyyy-mm-dd");
-const Sup = require('../controllers/supplierController')
+const PERCENT = 100;
+const dateFormat = require('dateformat');
+const DATE = dateFormat(new Date(), "yyyy-mm-dd");
 
 exports.create = async (request, response) => {
-    let supplier = await Supplier.findById('5c03ae30d0672e27604aae23');
-    let customer = await Customer.findById('5c03aed3d0672e27604aae26');
-    let project = await Project.findById('5c03ad90d0672e27604aae22');
+    let supplier = await Supplier.findById('5bffa0092b6cf93abcee1698');
+    let customer = await Customer.findById('5bff94f334c7e54ae054544c');
+    let project = await Project.findById('5bffce348e223a4eacc1d166');
     let invoice = new Invoice({
         invoiceSer: request.body.invoiceSer,
         invoiceNumber: request.body.invoiceNumber,
-        invoiceDate:  DATE,
+        invoiceDate: DATE,
         invoiceProjectQuant: request.body.invoiceProjectQuant,
         invoiceSupplier: supplier,
         invoiceCustomer: customer,
         invoiceProject: project,
         invoicePrice: project.projectPrice * request.body.invoiceProjectQuant,
-        invoiceVAT: ((project.projectPrice * request.body.invoiceProjectQuant) / 100 * INVOICE_VAT),
-        invoiceRate: (project.projectPrice * request.body.invoiceProjectQuant) + ((project.projectPrice * request.body.invoiceProjectQuant) / 100 * INVOICE_VAT),
+        invoiceVAT: ((project.projectPrice * request.body.invoiceProjectQuant) / PERCENT * INVOICE_VAT),
+        invoiceRate: (project.projectPrice * request.body.invoiceProjectQuant) + ((project.projectPrice * request.body.invoiceProjectQuant) / PERCENT * INVOICE_VAT),
         invoiceIsActive: request.body.invoiceIsActive
     });
-
-    console.log('Return : ' + JSON.stringify(invoice))
-    console.log('Date : ' + JSON.stringify(DATE))
-    invoice.save((err ) => {
+    //    console.log('Return : ' + JSON.stringify(invoice))
+    invoice.save((err) => {
         if (err) {
             return response.send();
         }
-      
         response.send('Record successfully created.');
     });
 }
