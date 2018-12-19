8c9164dd87e14d2a320a2e732e27152b9748980d
DESKTOP-S3DQCA1\Administrator <ridu196412@gmail.com>
2018-12-19 17:26:59 +0200

    changes

--- app/components/DropDown/index.js
+++ app/components/DropDown/index.js
@@ -3,27 +3,42 @@ import Style from './styles.css';
 import Select from 'react-select';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const techCompanies = [
-    { label: "first", value: 1 },
-    { label: "Second", value: 2 },
-    { label: "Third", value: 3 },
+const listClasses = [
+    { label: "First", value: 1, className: 'first' },
+    { label: "Second", value: 2, className: 'second' },
+    { label: "Third", value: 3, className: 'third' },
 ];
 export default class DropDown extends Component {
 
     state = {
-
-        value: 1
+        selectedOption: null,
+        cl: ''
     }
 
+    handleChange = (selectedOption) => {
+        this.setState({ selectedOption });
+        let cl = String(selectedOption.className)
+        console.log(`Option selected:`, cl);
+    }
     render() {
+        const { selectedOption } = this.state;
+        const name = Style.this.cl;
         return (
             <div>
                 <div className={Style.main}>Drop Down</div>
                 <div className="container">
                     <div className="row">
                         <div className="col-md-4"></div>
-                        <div className="col-md-4">
-                            <Select options={techCompanies} />
+                        <div className="col-md-4 ">
+                            <div className={name}>
+
+                                <Select
+                                    value={selectedOption}
+                                    onChange={this.handleChange}
+                                    options={listClasses}
+                                />
+                            </div>
+                            <div className={Style.cl}>Test</div>
                         </div>
                         <div className="col-md-4"></div>
                     </div>
