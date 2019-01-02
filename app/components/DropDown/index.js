d1cbc2eb6a3f24964718e3dd2b6f5dc46c77cc6e
DESKTOP-S3DQCA1\Administrator <ridu196412@gmail.com>
2019-01-02 14:20:34 +0200

    drop down list

--- app/components/DropDown/index.js
+++ app/components/DropDown/index.js
@@ -2,35 +2,41 @@ import React, { Component } from 'react';
 import Style from './styles.css';
 import Select from 'react-select';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 
 const listClasses = [
     { label: "First", value: 1, className: 'first' },
     { label: "Second", value: 2, className: 'second' },
     { label: "Third", value: 3, className: 'third' },
 ];
+
 export default class DropDown extends Component {
 
     state = {
         selectedOption: null,
-        cl: ''
+        x: ""
     }
 
     handleChange = (selectedOption) => {
         this.setState({ selectedOption });
         let cl = String(selectedOption.className)
+        this.x = cl;
         console.log(`Option selected:`, cl);
     }
+
     render() {
         const { selectedOption } = this.state;
-        const name = Style.this.cl;
+        console.log("Selected  :" + this.x)
+        const xx = ('Style.' + this.x)
+        console.log("Selected  +  :" + xx)
         return (
             <div>
-                <div className={Style.main}>Drop Down</div>
+                <div className={Style.first} >Drop Down</div>
                 <div className="container">
                     <div className="row">
                         <div className="col-md-4"></div>
                         <div className="col-md-4 ">
-                            <div className={name}>
+                            <div className={`${this.x}`}>
 
                                 <Select
                                     value={selectedOption}
@@ -38,7 +44,7 @@ export default class DropDown extends Component {
                                     options={listClasses}
                                 />
                             </div>
-                            <div className={Style.cl}>Test</div>
+                            <div className={`${this.x}`}>Test</div>
                         </div>
                         <div className="col-md-4"></div>
                     </div>
--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -34,19 +34,23 @@ module.exports = options => ({
         },
       },
       {
-        // Preprocess our own .css files
-        // This is the place to add your own loaders (e.g. sass/less etc.)
-        // for a list of loaders, see https://webpack.js.org/loaders/#styling
+        //   // Preprocess our own .css files
+        //   // This is the place to add your own loaders (e.g. sass/less etc.)
+        //   // for a list of loaders, see https://webpack.js.org/loaders/#styling
         test: /\.css$/,
         exclude: /node_modules/,
         use: [
+
           'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-            },
-          },
+
+          // {
+          //   loader: 'css-loader',
+          //   options: {
+          //     modules: true,
+          //   },
+          // },
+          'css-loader',
+
         ],
       },
       {
